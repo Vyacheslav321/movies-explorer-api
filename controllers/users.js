58be795f7fb9c36da2da1c82787a445b1571f6d1
Vyacheslav321 <apxsale@yandex.ru>
2023-01-17 07:39:44 +0300

    fix: correct user data

--- controllers/users.js
+++ controllers/users.js
@@ -8,18 +8,22 @@ const NotValidError = require('../errors/NotValidError');
 const User = require('../models/user');
 const { JWT_KEY } = require('../utils/config');
 const {
-  ALREDY_EXIST_USER_EMAIL, USER_NOT_FOUND, BAD_REQUEST_ERROR, AUTH_BAD_PASSWORD,
+  ALREDY_EXIST_USER_EMAIL,
+  USER_NOT_FOUND,
+  BAD_REQUEST_ERROR,
+  AUTH_BAD_PASSWORD,
 } = require('../utils/errors');
 
 // контроллер регистрации
 module.exports.createUser = (req, res, next) => {
-  const {
-    email, password, name,
-  } = req.body;
+  const { email, password, name } = req.body;
   User.findOne({ email });
-  bcrypt.hash(password, 10)
+  bcrypt
+    .hash(password, 10)
     .then((hash) => User.create({
-      name, email, password: hash,
+      name,
+      email,
+      password: hash,
     }))
     .then((userData) => res.send({
       email: userData.email,
@@ -39,7 +43,8 @@ module.exports.createUser = (req, res, next) => {
 // контроллер login
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
-  User.findOne({ email }).select('+password')
+  User.findOne({ email })
+    .select('+password')
     .then((user) => {
       if (!user) {
         throw new NotValidError(USER_NOT_FOUND); // 401
@@ -48,11 +53,7 @@ module.exports.login = (req, res, next) => {
         if (!isValidPassword) {
           return next(new NotValidError(AUTH_BAD_PASSWORD)); // 401
         }
-        const token = jwt.sign(
-          { _id: user._id },
-          JWT_KEY,
-          { expiresIn: '7d' },
-        );
+        const token = jwt.sign({ _id: user._id }, JWT_KEY, { expiresIn: '7d' });
         return res.send({ token });
       });
     })
@@ -79,30 +80,23 @@ module.exports.getCurrentUser = (req, res, next) => {
 // сработает при PATCH запросе на URL /users/me
 module.exports.updateProfile = (req, res, next) => {
   const { email, name } = req.body;
-  User.findOne({ email })
-    .then((data) => {
-      if (data) {
-        return next(new AlreadyExistDataError(ALREDY_EXIST_USER_EMAIL)); // 409
-      }
-      return User.findByIdAndUpdate(
-        req.user._id,
-        { email, name },
-        { runValidators: true, new: true },
-      )
-        .orFail(new NotFoundError(USER_NOT_FOUND)) // 404
-        .then((userData) => res.send({
-          email: userData.email,
-          name: userData.name,
-        }))
-        .catch((err) => {
-          if (err.name === 'ValidationError') {
-            next(new BadRequestError(BAD_REQUEST_ERROR));
-          } else {
-            next(err);
-          }
-        });
-    })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { email, name },
+    { runValidators: true, new: true },
+  )
+    .orFail(new NotFoundError(USER_NOT_FOUND)) // 404
+    .then((userData) => res.send({
+      email: userData.email,
+      name: userData.name,
+    }))
     .catch((err) => {
-      next(err);
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError(BAD_REQUEST_ERROR));
+      } else if (err.code === 11000) {
+        next(new AlreadyExistDataError(ALREDY_EXIST_USER_EMAIL)); // 409
+      } else {
+        next(err);
+      }
     });
 };
--- middlewares/limiter.js
+++ middlewares/limiter.js
@@ -3,7 +3,7 @@ const { RATE_LIMIT } = require('../utils/errors');
 
 const limiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 1000,
+  max: 1000000000,
   message: RATE_LIMIT,
 });
 
